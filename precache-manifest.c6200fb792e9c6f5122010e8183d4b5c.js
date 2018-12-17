ee1fabd4d039fbba947fcd6b3af6f3bf12c91584
cris-ech <i42runac@uco.es>
2018-12-17 11:42:20 +0100

    Updates

---
+++ precache-manifest.c6200fb792e9c6f5122010e8183d4b5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c48ae1d05b9db76c0f2beb50935b7ab",
+    "url": "/restaurantes-react/static/media/2.9c48ae1d.jpg"
+  },
+  {
+    "revision": "6cc54330acc830bd94ae",
+    "url": "/restaurantes-react/static/css/main.22404584.chunk.css"
+  },
+  {
+    "revision": "096c6a7951aaf8c7e4971fc2a67efa2f",
+    "url": "/restaurantes-react/static/media/hero-min.096c6a79.jpg"
+  },
+  {
+    "revision": "392b66ad23fe93efdfb9",
+    "url": "/restaurantes-react/static/js/1.392b66ad.chunk.js"
+  },
+  {
+    "revision": "328d9762d3bdd0e8090e",
+    "url": "/restaurantes-react/static/js/runtime~main.328d9762.js"
+  },
+  {
+    "revision": "5b40cf5e7dd7afbc43181fa2a931f1c9",
+    "url": "/restaurantes-react/static/media/1.5b40cf5e.jpg"
+  },
+  {
+    "revision": "6cc54330acc830bd94ae",
+    "url": "/restaurantes-react/static/js/main.6cc54330.chunk.js"
+  },
+  {
+    "revision": "54bb0235511e23baf332dd786b4409ab",
+    "url": "/restaurantes-react/static/media/3.54bb0235.jpg"
+  },
+  {
+    "revision": "8a707fb9123289df6e43af2e0cb9af03",
+    "url": "/restaurantes-react/static/media/logo.8a707fb9.png"
+  },
+  {
+    "revision": "00408e40fc7cf61b7f99dcb4398462d7",
+    "url": "/restaurantes-react/static/media/Oferta.00408e40.png"
+  },
+  {
+    "revision": "392b66ad23fe93efdfb9",
+    "url": "/restaurantes-react/static/css/1.db318142.chunk.css"
+  },
+  {
+    "revision": "4fb62371abfaa29d6b1783597141a0e7",
+    "url": "/restaurantes-react/index.html"
+  }
+];
